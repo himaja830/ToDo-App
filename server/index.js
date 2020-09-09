84eb274c03aaecc16c2a28f8ba73e7d4fb12c967
Himaja830 <Himaja.Sriramaneni@gmail.com>
2020-09-09 13:07:49 -0500

    initialized server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ const express = require('express');
  
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.get('/',(req,res) => res.json({hello: 'world'}));
+
+//Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+
 app.listen(PORT,function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
   });
